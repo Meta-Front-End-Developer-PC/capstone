b9e59f7681d367b0d4c9557ebda3c5950e999c73
Thomas Cashman <cashmant@sureskills.com>
2022-12-19 16:44:33 +0000

    Seed random value based on date

--- api.js
+++ api.js
@@ -1,10 +1,21 @@
+const seededRandom = function (seed) {
+    var m = 2**35 - 31;
+    var a = 185852;
+    var s = seed % m;
+    return function () {
+        return (s = s * a % m) / m;
+    };
+}
+
 const fetchAPI = function(date) {
     let result = [];
+    let random = seededRandom(date.getDate());
+
     for(let i = 17; i <= 23; i++) {
-        if(Math.random() < 0.5) {
+        if(random() < 0.5) {
             result.push(i + ':00');
         }
-        if(Math.random() < 0.5) {
+        if(random() < 0.5) {
             result.push(i + ':30');
         }
     }
